8b1c61c3a4a3a39f0b7fc5a72fc924161f8f8dd0
DashDaSmash <160383178+DashDaSmash@users.noreply.github.com>
2024-07-12 19:18:29 +0530

    1.0.17
    + added code to fetch scores and show it in profile screen

--- lib/Screens/ProfileScreen.dart
+++ lib/Screens/ProfileScreen.dart
@@ -1,8 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:learn_py/main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   // Initialize any state variables or perform other setup here
+  //   // For example, fetch data from Firestore
+  //   _fetchUserData();
+  // }
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  // Add your state variables and methods here
+  int userTotalScore = 0;
+  int quizCount = 0;
+  int averageScore = 0;
+  int lastTestScore = 0;
+
+  Future<void> _fetchUserData() async {
+    final userRef =
+        FirebaseFirestore.instance.collection('users').doc(userEmail);
+    final userDoc = await userRef.get();
+    final Map<String, dynamic> quizScores = userDoc.data()?['QuizScores'] ?? {};
+    print('quizScores: $quizScores');
+    for (final key in quizScores.keys) {
+      lastTestScore = int.parse(quizScores[key].toString());
+      userTotalScore += lastTestScore;
+      quizCount++;
+      double average = userTotalScore / quizCount;
+      averageScore = average.round();
+    }
+    setState(() {});
+    print('userTotalScore: $userTotalScore');
+    print('quizCount: $quizCount');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +55,8 @@ class ProfileScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text('Last test score: $lastTestScore'),
+            Text('averageScore: $averageScore'),
             Text('Email: $userEmail'),
             ElevatedButton(
               onPressed: () {
